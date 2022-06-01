8baca08ede50e522109a1af773b73700e5e398a0
Corey Koelewyn <ckoelewyn@uvic.ca>
2022-06-01 09:59:45 -0700

    boilerplate from start.spring.io

---
+++ src/test/java/org/uvic/CICD_intro/CicdIntroApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uvic.CICD_intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdIntroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
